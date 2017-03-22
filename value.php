dc9a38bfbd1bc97b06c684b2dbbc7e0fcba588c9
zainaqdas1 <lovershostbuddy@gmail.com>
2017-03-22 18:22:18 +0500

    Update value.php

--- value.php
+++ value.php
@@ -20,11 +20,7 @@ $gwe=getUrl('/me',$tk,array(
 'fields' => 'id,name',
 ));
 
-$site=' ♥ c h α υ δ h r ψ s 🌹 β ο τ τ ε r ♥
-
-♡[●<~|| chaudhrys-web.tk ||~>●]♡
-
-♥♡o. Cɾεαtεɾ 💢 zɑíղ ɑզժɑՏ .o♡ ♥  
+$site=' ❤ CHAUDHRYS BOTTER ❤ ZAINU ❤ AMEE ❤ SALLU ❤ ZAINPATHAN.TK ❤  
  ';
 $true=$ops.'
  '.$site;
